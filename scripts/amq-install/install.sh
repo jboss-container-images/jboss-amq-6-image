7268152d1d493d66aeae235407571ff61a21451e
Marek Goldmann <marek.goldmann@gmail.com>
2017-05-19 10:49:50 +0200

    Import

---
+++ scripts/amq-install/install.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Installs the AMQ distribution into the filesystem.
+set -e
+
+SOURCES_DIR=/tmp/artifacts/
+DISTRIBUTION_VERSION="jboss-a-mq-6.3.0.redhat-187"
+ACTIVEMQ_VERSION="apache-activemq-5.11.0.redhat-630187"
+
+unzip -q "$SOURCES_DIR/${DISTRIBUTION_VERSION}.zip"
+
+pushd $DISTRIBUTION_VERSION/extras > /dev/null
+  unzip -q "${ACTIVEMQ_VERSION}-bin.zip"
+  mv $ACTIVEMQ_VERSION $AMQ_HOME
+popd > /dev/null
+
+rm -rf $DISTRIBUTION_VERSION
---
+++ tests/features/amq.feature
@@ -0,0 +1,6 @@
+Feature: Standalone AMQ image tests
+
+  @ci
+  Scenario: Check that the jboss-amq-6/amq image contains 5 layers
+    Given image is built
+     Then image should contain 5 layers
