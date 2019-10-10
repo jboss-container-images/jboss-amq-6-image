56a458a6109132a97bd883592e318d8fcd2f036e
Howard Gao <howard.gao@gmail.com>
2019-10-10 23:40:15 +0800

    update to new version

--- modules/amq-install/install.sh
+++ modules/amq-install/install.sh
@@ -3,8 +3,8 @@
 set -e
 
 SOURCES_DIR=/tmp/artifacts/
-DISTRIBUTION_VERSION="jboss-a-mq-6.3.0.redhat-396"
-ACTIVEMQ_VERSION="apache-activemq-5.11.0.redhat-630396"
+DISTRIBUTION_VERSION="jboss-a-mq-6.3.0.redhat-416"
+ACTIVEMQ_VERSION="apache-activemq-5.11.0.redhat-630416"
 
 unzip -q "$SOURCES_DIR/${DISTRIBUTION_VERSION}.zip"
 
