2fb9c952d6ef302b90e50f9b22869b76b1ddd141
Howard Gao <howard.gao@gmail.com>
2019-11-01 15:24:43 +0800

    ENTMQ-2339 Update image.yaml to work with latest CEKit 3.4.0 for integration with CPaaS

--- modules/amq-install/install.sh
+++ modules/amq-install/install.sh
@@ -6,7 +6,11 @@ SOURCES_DIR=/tmp/artifacts/
 DISTRIBUTION_VERSION="jboss-a-mq-6.3.0.redhat-416"
 ACTIVEMQ_VERSION="apache-activemq-5.11.0.redhat-630416"
 
+AMQ_HOME=/opt/amq
+
 unzip -q "$SOURCES_DIR/${DISTRIBUTION_VERSION}.zip"
+
+ls $SOURCES_DIR
 
 pushd $DISTRIBUTION_VERSION/extras > /dev/null
   unzip -q "${ACTIVEMQ_VERSION}-bin.zip"
