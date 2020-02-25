245ac470244204557d8512230f9c608fb6b92b66
Howard Gao <howard.gao@gmail.com>
2020-02-25 23:57:48 +0800

    R15 build

--- modules/amq-install/install.sh
+++ modules/amq-install/install.sh
@@ -3,8 +3,8 @@
 set -e
 
 SOURCES_DIR=/tmp/artifacts/
-DISTRIBUTION_VERSION="jboss-a-mq-6.3.0.redhat-424"
-ACTIVEMQ_VERSION="apache-activemq-5.11.0.redhat-630424"
+DISTRIBUTION_VERSION="jboss-a-mq-6.3.0.redhat-434"
+ACTIVEMQ_VERSION="apache-activemq-5.11.0.redhat-630434"
 
 AMQ_HOME=/opt/amq
 
