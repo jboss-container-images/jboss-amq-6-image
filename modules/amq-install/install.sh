755eb34a4477672c5eab12696cffa912387e0609
Howard Gao <howard.gao@gmail.com>
2022-03-07 22:19:29 +0800

    Update amq to R20

--- modules/amq-install/install.sh
+++ modules/amq-install/install.sh
@@ -3,8 +3,8 @@
 set -e
 
 SOURCES_DIR=/tmp/artifacts/
-DISTRIBUTION_VERSION="jboss-a-mq-6.3.0.redhat-495"
-ACTIVEMQ_VERSION="apache-activemq-5.11.0.redhat-630495"
+DISTRIBUTION_VERSION="jboss-a-mq-6.3.0.redhat-516"
+ACTIVEMQ_VERSION="apache-activemq-5.11.0.redhat-630516"
 
 AMQ_HOME=/opt/amq
 
